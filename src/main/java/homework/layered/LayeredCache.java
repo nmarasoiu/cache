87cd19f5eaeb5cf9a48388714407d8e63c3191bc
dnmaras <nicolae.marasoiu@gmail.com>
2014-10-28 23:44:52 +0200

    func remove

--- src/main/java/homework/layered/LayeredCache.java
+++ src/main/java/homework/layered/LayeredCache.java
@@ -8,6 +8,8 @@ import homework.option.Option;
 import homework.utils.LazyValue;
 import homework.utils.Pair;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
 
@@ -15,10 +17,12 @@ import java.util.stream.Stream;
 public class LayeredCache<K, V> implements FunctionalCache<K, V> {
     protected final StatAwareFuncCache<K, V> memCache;
     protected final StatAwareFuncCache<K, V> fsCache;
+    protected final List<StatAwareFuncCache<K, V>> caches;
 
     public LayeredCache(StatAwareFuncCache<K, V> memCache, StatAwareFuncCache<K, V> fsCache) {
         this.memCache = memCache;
         this.fsCache = fsCache;
+        caches = Arrays.asList(memCache, fsCache);
     }
 
     @Override
@@ -30,37 +34,40 @@ public class LayeredCache<K, V> implements FunctionalCache<K, V> {
                 super(cache, callback);
             }
 
-            public Option<Statistic<V>> getCachedValue() {
+            Option<Statistic<V>> getCachedValueWithStatisticIfAny() {
                 return cachedValueIfAny.getValue();
             }
+
+            Statistic<V> getCachedValueWithStatistic() {
+                return getCachedValueWithStatisticIfAny().get();
+            }
         }
-        Option<CacheAndCallback> cacheHitAndCallbackIfAny = Option.from(
-                Stream.of(
+        Option<CacheAndCallback> cacheHitAndCallbackIfAny =
+                Option.from(Stream.of(
                         new CacheAndCallback(memCache, statistic -> {
-                        }),
-                        new CacheAndCallback(fsCache, statistic -> memCache.put(key, statistic))
-                )
-                        .filter(pair -> pair.getCachedValue().isPresent())
+                        }), new CacheAndCallback(fsCache, statistic -> memCache.put(key, statistic)))
+                        .filter(pair -> pair.getCachedValueWithStatisticIfAny().isPresent())
                         .findFirst());
 
         //execute callback if any
-        cacheHitAndCallbackIfAny.ifPresent(pair -> pair.getSecond().accept(pair.getCachedValue().get()));
+        cacheHitAndCallbackIfAny.ifPresent(pair -> pair.getSecond().accept(pair.getCachedValueWithStatistic()));
 
         //select just the cache hit if any (discard callback, unwrap)
-        return cacheHitAndCallbackIfAny.map(pair -> pair.getCachedValue().get()).map(stat -> stat.getValue());
+        return cacheHitAndCallbackIfAny.map(pair -> pair.getCachedValueWithStatistic().getValue());
     }
 
 
     @Override
     public synchronized void put(K key, V value) {
-        memCache.toSimpleCache().put(key, value);
-        fsCache.toSimpleCache().put(key, value);
+        caches.forEach(cache -> cache.toSimpleCache().put(key, value));
     }
 
     @Override
-    public synchronized boolean remove(K k) {
-        memCache.remove(k);
-        return fsCache.remove(k);
+    public synchronized boolean remove(K key) {
+        return caches.stream()
+                .map(cache -> cache.remove(key))
+                .max((o1, o2) -> 1)
+                .get();
     }
 
     @Override
