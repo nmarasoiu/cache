282d2f27604297c7302ca863ebb901bd7d265a3c
dnmaras <nicolae.marasoiu@gmail.com>
2014-10-28 22:49:35 +0200

    simp

--- src/main/java/homework/utils/LazyValue.java
+++ src/main/java/homework/utils/LazyValue.java
@@ -5,20 +5,19 @@ import homework.option.Option;
 import java.util.function.Supplier;
 
 import static homework.option.Option.none;
-import static homework.option.Option.some;
 
 /**
  * Created by nmarasoiu on 10/28/2014.
  */
 public class LazyValue<V> {
     //I want to cover the scenario when the client can emitter of the lazy value can delegate to its consumer the supplier of the value, if any
-    private final Option<Supplier<Option<V>>> supplierOption;
+    private final Supplier<Option<V>> supplier;
 
     //I want to cover value==null case
     private Option<V> valueOption = none();
 
-    public LazyValue(Supplier<Option<V>> supplierOption) {
-        this.supplierOption = some(supplierOption);
+    public LazyValue(Supplier<Option<V>> supplier) {
+        this.supplier = supplier;
     }
 
     public LazyValue() {
@@ -27,7 +26,7 @@ public class LazyValue<V> {
 
     public Option<V> getValue() {
         if (valueOption.isEmpty()) {
-            valueOption = supplierOption.flatMap(supplier -> supplier.get());
+            valueOption = supplier.get();
         }
         return valueOption;
     }
