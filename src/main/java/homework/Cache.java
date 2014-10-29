14e785da3d3f7c9a4498fdba7e9cf52123e375ea
nmarasoiu <nmarasoiu@computas.com>
2014-10-29 14:38:05 +0200

    todos

--- src/main/java/homework/Cache.java
+++ src/main/java/homework/Cache.java
@@ -2,6 +2,16 @@ package homework;
 
 /**
  * Created by dnmaras on 10/25/14.
+ * todo 1) There should correct exception handling implemented - i.e. not silent
+ * --> poate fi o optiune a cachului: daca sa inghita exceptii (pe care sa le logheze) , sau sa le lase sa iasa imediat, sau sa impacheteze 2 exceptii si sa iasa
+ 2) If a standard implementation of the task is taken (copy-paste, import) - there is no point in the task. We would prefer to have it implemented using core java libraries.
+ --> totusi un injector trebuie bagat! guice looks ok?
+ 3) The solution will be mostly evaluated based on: covered with tests, code quality. If there are some corner cases not covered - it is ok.
+ --> macar cateva cazuri cheie, cu mocking pe bune
+
+ de testat de mana multiple entries per hash si concurenta
+
+ eviction in memory si daca e de replicat coada pe filesystem fie implicit fie explicit
  */
 public interface Cache<K, V> {
     V get(K key);
